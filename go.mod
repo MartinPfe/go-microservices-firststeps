56172a62bd0023ce9240ab4040c1054f14ee75c6
Martin <martin.pfe@gmail.com>
2021-11-04 00:15:32 -0300

    second class done

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/MartinPfe/go-microservices-firststeps
+
+go 1.17
--- main.go
+++ main.go
@@ -1,22 +1,57 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/MartinPfe/go-microservices-firststeps/handlers"
 )
 
 func main() {
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+
+	//Armamos los 2 hadnlers 
 	hh := handlers.NewHellow(l)
 	gh := handlers.NewGoodbye(l)
 
+	//Creamos el servidor, que va a contener nuestros handlers
 	sm := http.NewServeMux()
 
+	//Armamos las rutas para cada uno de los handlers
 	sm.Handle("/", hh)
 	sm.Handle("/goodbye", gh)
 
-	http.ListenAndServe("localhost:9090", sm)
+	//Config del servidor
+	s:= &http.Server{
+		Addr: 			"localhost:9090",
+		Handler: 		sm,
+		IdleTimeout:  	120 *time.Second,
+		ReadTimeout: 	1 * time.Second,
+		WriteTimeout: 	1 * time.Second,
+	}
+
+	//Lo iniciamos en una goroutine para despues poder terminarlos
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	//Seteamos que los channels van a esperar a que llegue una señal de kill
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	//Cuando llega la señal de kill, retomamso
+	sig := <- sigChan
+	l.Println("Senal de de fin recibida", sig)
+
+	//Matamos el server
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
 }
